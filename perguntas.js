ba37870850f73c6b6c833915423cdf41a4e81475
adryy00 <160855134+adryy00@users.noreply.github.com>
2024-12-02 11:00:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programaçao',
+    'O que e JavaScripit?',
+    'O Javascrpit e uma linguagem d programaçao'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França e Paris.'
+)
+
+criaCartao(
+    'Programaçao',
+    'O que e uma funçao',
+    'Uma funçao e um bloco de codigo que executa uma tarefa.'
+)
+
+criaCartao(
+    'Linguaa Inglesa',
+    'Como se diz oi em ingles?',
+    'Oi em ingles se di HI (RAI'
+)
